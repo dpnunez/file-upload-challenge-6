d18ea9aae0ee001a4034c2518c40f8600fe695ca
Daniel Pôrto Núñez <danielportonunez@MacBook-Pro-de-Daniel.local>
2020-11-18 20:06:45 -0300

    feat: create transtion / category feat's

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,17 +1,74 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column()
   value: number;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
   category_id: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
 export default Transaction;
+
+// import {
+//   Entity,
+//   Column,
+//   PrimaryGeneratedColumn,
+//   CreateDateColumn,
+//   UpdateDateColumn,
+//   ManyToOne,
+//   JoinColumn,
+// } from 'typeorm';
+
+// import User from './User';
+
+// // Toda a instancia de Appointment será salva na tabela "appointments"
+// @Entity('appointments')
+// class Appointment {
+//   @PrimaryGeneratedColumn('uuid')
+//   id: string;
+
+//   @Column()
+//   provider_id: string;
+
+//   @ManyToOne(() => User)
+//   @JoinColumn({ name: 'provider_id' })
+//   provider: User;
+
+//   @Column('timestamp with time zone')
+//   date: Date;
+
+//   @CreateDateColumn()
+//   created_at: Date;
+
+//   @UpdateDateColumn()
+//   updated_at: Date;
+// }
+
+// export default Appointment;
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,63 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import Category from '../models/Category';
 
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+
+    // Data validation
+    if (!['income', 'outcome'].includes(type)) {
+      throw new AppError('Tipo invalido', 400);
+    }
+
+    // Category verification
+    const categoryAlreadyExist = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    if (categoryAlreadyExist) {
+      const { id: category_id } = categoryAlreadyExist;
+      const transaction = transactionRepository.create({
+        title,
+        value,
+        type,
+        category_id,
+      });
+      await transactionRepository.save(transaction);
+
+      return transaction;
+    }
+
+    // Category creation
+    const createdCategory = categoryRepository.create({ title: category });
+    const { id: category_id } = await categoryRepository.save(createdCategory);
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category_id,
+    });
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
